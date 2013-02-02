b118880aeeb0a918127f8fc87ddfa096fff6463a
ahncorp <ahncorp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-02 18:52:27 +0000

    Imported new version
    git-svn-id: https://plugins.svn.wordpress.org/ahn-feedsyndicate-news-content-plugin/trunk@662534 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ahn-feedsyndicate-news-content-plugin/FeedSyndicateFeeds.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: FeedSyndicate Feeds
+Plugin URI: http://www.feedsyndicate.com/software
+Description: Plugin to import data from NewsML to wordpress database. Copyright FeedSyndicate
+Version: 3.0
+Author: FeedSyndicate
+Author URI: http://www.feedsyndicate.com
+*/
+
+define( 'FeedSyndicateFeeds_PATH', dirname( __FILE__ ) );
+define( 'FeedSyndicateFeeds_URL',  plugins_url( '', __FILE__ ) );
+
+require FeedSyndicateFeeds_PATH . '/helpers.php';
+require FeedSyndicateFeeds_PATH . '/cron_functions.php';
+require FeedSyndicateFeeds_PATH . '/classes/FeedSyndicateFeeds.class.php';
+require FeedSyndicateFeeds_PATH . '/classes/FeedSyndicateFeedsTable.class.php';
+require FeedSyndicateFeeds_PATH . '/classes/FeedSyndicateNewsML.class.php';
+
+load_plugin_textdomain( 'FeedSyndicateFeeds', false, FeedSyndicateFeeds_URL );
+
+if ( !class_exists( 'FeedSyndicateAJAX' ) )
+	require 'classes/FeedSyndicateAJAX.class.php';
+
+if ( !class_exists( 'FeedSyndicateAdmin' ) )
+	require 'classes/FeedSyndicateAdmin.class.php';
+
+new FeedSyndicateFeeds();
+
+
+
+
+
+
+
