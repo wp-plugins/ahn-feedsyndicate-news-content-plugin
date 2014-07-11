7a591cb123ffac86414723aa55a48ad734b703c7
ahncorp <ahncorp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 12:31:10 +0000

    Upgrade
    git-svn-id: https://plugins.svn.wordpress.org/ahn-feedsyndicate-news-content-plugin/tags/3.5.1@946860 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+if (!function_exists("FeedSyndicateRemoteImageGrabber")){
+
+    // Function from remote-image-grabber plugin
+    function FeedSyndicateRemoteImageGrabber($post, $url){
+
+		$url2= str_replace('&amp;', '&', $url);
+		$upload = wp_upload_dir($post['post_date']);
+		preg_match('/[a-z0-9%_\.-]+\.(jpg|jpeg|gif|png)/i', $url2, $pu);
+		$file_name= $pu[0];
+
+		$upload2 = wp_upload_bits( $file_name, 0, '', $post['post_date']);
+
+		if ( $upload2['error'] ) {
+			echo $upload2['error'];
+			return new WP_Error( 'upload_dir_error', $upload2['error'] );
+		}
+
+		$headers = wp_get_http($url2, $upload2['file']);
+
+		if ( ! $headers ) {
+			@unlink($upload2['file']);
+			return new WP_Error( 'import_file_error', __('Remote server did not respond', 'FeedSyndicateFeeds') );
+		}
+
+		if ( $headers['response'] != '200' ) {
+			@unlink($upload2['file']);
+			return new WP_Error( 'import_file_error', sprintf(__('Remote server says: %1$d %2$s', 'FeedSyndicateFeeds'), $headers['response'], get_status_header_desc($headers['response']) ) );
+		}
+		elseif ( isset($headers['content-length']) && filesize($upload2['file']) != $headers['content-length'] ) {
+			@unlink($upload2['file']);
+			return new WP_Error( 'import_file_error', __('Remote file is incorrect size', 'FeedSyndicateFeeds') );
+		}
+
+		$upload2['content-type']= $headers['content-type'];
+		return $upload2;
+    }
+}
+?>
